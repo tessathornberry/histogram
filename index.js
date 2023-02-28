992ef4439b36c22401a1f543c36425632e25ea36
Tessa Thornberry <tthornberryclass@gmail.com>
2023-02-27 16:28:55 -0800

    successfully logging user data as inputted and converted to numbers

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
+function getData(form) {
+  var formData = new FormData(form);
+  console.log(Object.fromEntries(formData).name); //input from form
+  let StringOfNumbers = Object.fromEntries(formData).name;
+  let arrayOfStrings = StringOfNumbers.split(',');
+  console.log(arrayOfStrings);
+  for (var i = 0; i < arrayOfStrings.length; i++) {
+    console.log(Number.parseInt(arrayOfStrings[i]));
+  }
+}
+
+document.getElementById("form-input").addEventListener("submit", function (e) {e.preventDefault();
+getData(e.target);
+});
+
 const canvas = document.getElementById('canvas');
 const context = canvas.getContext('2d');
-// console.log(context);
-
-
 context.fillStyle = 'green';
 context.fillRect(20, 30, 40, 50);
 context.fillRect(62, 30, 40, 100);
