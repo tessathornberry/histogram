8de88404f3193a326f3f18a0651fa47029730060
Tessa Thornberry <tthornberryclass@gmail.com>
2023-02-28 16:31:44 -0800

    labeling histogram

--- index.js
+++ index.js
@@ -9,19 +9,35 @@ var numbers = null;
 document.getElementById("form-input").addEventListener("submit", function (e) {
   e.preventDefault();
   numbers = getData(e.target);
-  looseGraph(numbers);
+  histogram(numbers);
 });
 
 document.getElementById("form-input").addEventListener("reset", function () {
   numbers = null;
-  looseGraph(numbers);
+  histogram(numbers);
 });
 
-function looseGraph(inputString) {
+var canvasWidth = 0;
+var canvas2 = document.querySelector('canvas');
+fitToContainer(canvas2);
+
+function fitToContainer(canvas2){
+  // Make it visually fill the positioned parent
+  canvas2.style.width ='100%';
+  canvas2.style.height='100%';
+  // ...then set the internal size to match
+  canvas2.width  = canvas2.offsetWidth;
+  canvas2.height = canvas2.offsetHeight;
+  canvasWidth = canvas2.offsetWidth;
+}
+
+
+function histogram(inputString) {
   inputString = inputString || '5, 14, 14, 14, 14, 50, 50, 50, 100, 100, 100, 100, 100, 100, 100, 100, 200, 200'; //default values
   let inputArray = inputString.split(',');
   let maxFrequency = 0;
   let object = {};
+
   for (let i = 0; i < inputArray.length; i++) {
     inputArray[i] = parseFloat(inputArray[i]);
     if (inputArray[i] >=0 || inputArray[i] <0) { //gets rid of non-number items
@@ -30,12 +46,12 @@ function looseGraph(inputString) {
       } else {
         object[inputArray[i]]++;
       }
-      } else {
-          alert('Please use only numbers, spaces, decimals, or commas in your entry. Please check for spaces after decimals.')
-      }
-      if (object[inputArray[i]] > maxFrequency) {
-        maxFrequency = object[inputArray[i]];
-      }
+    } else {
+        alert('Please use only numbers, spaces, decimals, or commas in your entry. Please check for spaces after decimals.')
+    }
+    if (object[inputArray[i]] > maxFrequency) {
+      maxFrequency = object[inputArray[i]];
+    }
   }
 
   console.log('object', object);
@@ -43,52 +59,34 @@ function looseGraph(inputString) {
   let context = canvas.getContext('2d');
   context.reset();
 
-  function compareNumbers(a, b) {
+  function compareNumbers(a, b) { //to ensure good sort
     return a - b;
   }
 
   let arrayOfKeys = Object.keys(object).sort(compareNumbers); //sorts keys just in case
+  let xAxis = arrayOfKeys.length;
+  let contextWidth = (canvasWidth - (xAxis * 5))/xAxis;
   console.log('arrayOfKeys', arrayOfKeys)
   console.log('maxFrequency', maxFrequency);
-
-  let placement = 0;
+  var gradient = context.createLinearGradient(100, 300, 100, 0);
+  gradient.addColorStop(0, "green");
+  gradient.addColorStop(1, "lightgreen");
+  let placement = 5;
   for (var j = 0; j < arrayOfKeys.length; j++) {
-    context.fillStyle = 'green';
-    context.fillRect(placement + 30, (maxFrequency * 20), 25, -(object[arrayOfKeys[j]] * 15));
-    placement += 30;
+
+    context.fillStyle = gradient;
+    context.fillRect(placement, 300, contextWidth, -(object[arrayOfKeys[j]]));
+    placement += (contextWidth + 5);
   }
 }
-looseGraph(numbers);
+histogram(numbers);
 
-/*  // let arrayOfStrings = StringOfNumbers.split(',');
-  // var arrayOfNumbers = []
-  // for (var i = 0; i < arrayOfStrings.length; i++) {
-  //   arrayOfNumbers.push(Number.parseInt(arrayOfStrings[i]));
-  //   console.log(Number.parseInt(arrayOfStrings[i]));
-  // }
+function assignGraph() {
+  var graph = document.getElementById('histogram');
+  var labels = document.getElementById('x-axis');
 
-  // function compareNumbers(a, b) {
-  //   return a - b;
-  // }
 
-  // return arrayOfNumbers.sort(compareNumbers); */
-// function looseGraph(array) {
-//   var array = array || [50, 100, 200, 5, 20, 5, 6, 8];
-//   console.log('array', array);
-//   let canvas = document.getElementById('canvas');;
-//   let context = canvas.getContext('2d');
-//   context.reset();
-//   let placement = 0;
-//   let maxValue = Math.max(...array);
-//   console.log('max', maxValue);
-//   for (var item in array) {
-//     console.log(array[item]);
-//     context.fillStyle = 'green';
-//     context.fillRect(placement + 30, (maxValue + 5), 25, -(array[item]));
-//     placement += 30;
-//   }
-// }
-// looseGraph(numbers);
+}
 
 
 
