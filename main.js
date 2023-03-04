e0133b79656acf07fa63cbbdfbfd7be90b595d11
Tessa Thornberry <tthornberryclass@gmail.com>
2023-03-04 11:54:29 -0800

    scrolling histogram

--- main.js
+++ main.js
@@ -12,28 +12,16 @@ document.getElementById("form-input").addEventListener("submit", function (e) {
   histogram(numbers);
 });
 
-document.getElementById("form-input").addEventListener("re", function () {
+document.getElementById("form-input").addEventListener("reset", function () {
   numbers = null;
   histogram(numbers);
 });
 
-var canvasWidth = 0;
-var canvas2 = document.querySelector('canvas');
-fitToContainer(canvas2);
-
-function fitToContainer(canvas2){
-  // Make it visually fill the positioned parent
-  canvas2.style.width ='100%';
-  canvas2.style.height='100%';
-  // ...then set the internal size to match
-  canvas2.width  = canvas2.offsetWidth;
-  canvas2.height = canvas2.offsetHeight;
-  canvasWidth = canvas2.offsetWidth;
-}
 
 
 function histogram(inputString) {
   inputString = inputString || '5, 14, 14, 14, 14, 50, 50, 50, 100, 100, 100, 100, 100, 100, 100, 100, 200, 200'; //default values
+  console.log('inputValues', inputString);
   let inputArray = inputString.split(',');
   let maxFrequency = 0;
   let object = {};
@@ -54,17 +42,12 @@ function histogram(inputString) {
     }
   }
   let takeTwo = document.getElementById("taketwo");
-  console.log(takeTwo.scrollWidth);
-  // takeTwo.innerHTML = '<h1>feelings</p><div class="block" style="width:50px;height:50px"></div>';
+  takeTwo.innerHTML = ''; //eliminates default blocks when values are submitted
 
 
   if (!allNumbers) {
     alert('Please use only numbers, spaces, decimals, or commas in your entry. Please check for spaces after decimals.')
   }
-  console.log('object', object);
-  let canvas = document.getElementById('canvas');;
-  let context = canvas.getContext('2d');
-  context.reset();
 
   function compareNumbers(a, b) { //to ensure good sort
     return a - b;
@@ -72,27 +55,10 @@ function histogram(inputString) {
 
   let arrayOfKeys = Object.keys(object).sort(compareNumbers); //sorts keys just in case
   let xAxis = arrayOfKeys.length;
-
   let takeTwoWidth = takeTwo.scrollWidth/xAxis; //this is the width of the bars
-  console.log('takeTwoWidth', takeTwoWidth);
 
-
-
-  let contextWidth = (canvasWidth - (xAxis * 5))/xAxis;
-  console.log('arrayOfKeys', arrayOfKeys)
-  console.log('maxFrequency', maxFrequency);
-  var gradient = context.createLinearGradient(100, 300, 100, 0);
-  gradient.addColorStop(0, "green");
-  gradient.addColorStop(1, "lightgreen");
-  let placement = 0;
-
-  // takeTwo.innerHtml += '<h1>feelings</h1><div class="block" style="width:'+ contextWidth + ';height:' + object[arrayOfKeys[j]] + '"></div>';
   for (var j = 0; j < arrayOfKeys.length; j++) {
-    // console.log(object[arrayOfKeys[j]]);
-    takeTwo.innerHTML += '<div class="block" style="width:'+ takeTwoWidth + 'px;height:' + (object[arrayOfKeys[j]] * 20) + 'px;background-color: blue;"><span class="divtext"><p>Value: ' + arrayOfKeys[j] + '</p>Frequency:' + object[arrayOfKeys[j]] + '</span></div>'
-    context.fillStyle = gradient;
-    context.fillRect(placement, 305, contextWidth, -(object[arrayOfKeys[j]]));
-    placement += (contextWidth + 5);
+    takeTwo.innerHTML += '<div class="block" style="width:'+ takeTwoWidth + 'px;height:' + (object[arrayOfKeys[j]] * 20) + 'px;background-color: blue;"><span class="divtext">Value: ' + arrayOfKeys[j] + '<br>Frequency:' + object[arrayOfKeys[j]] + '</span></div>'
   }
 }
 histogram(numbers);
